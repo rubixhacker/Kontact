5aa589ab017872b1a5f4f8082c66f120ccc86383
Stewart Boling <rubixhacker@gmail.com>
2017-12-30 22:18:20 -0600

    Added parsing of Relation to Kontact

---
+++ kontact/src/main/java/com/hackedcube/kontact/Relation.java
@@ -0,0 +1,60 @@
+package com.hackedcube.kontact;
+
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.support.annotation.Nullable;
+
+import com.gabrielittner.auto.value.cursor.ColumnAdapter;
+import com.gabrielittner.auto.value.cursor.ColumnName;
+import com.google.auto.value.AutoValue;
+import com.hackedcube.kontact.columnadapters.RelationTypeIntAdapter;
+
+@AutoValue
+public abstract class Relation {
+
+    @ColumnName(ContactsContract.CommonDataKinds.Relation.NAME)
+    public abstract String name();
+
+    @ColumnAdapter(RelationTypeIntAdapter.class)
+    @ColumnName(ContactsContract.CommonDataKinds.Phone.TYPE)
+    public abstract Relation.Type type();
+
+    @Nullable
+    @ColumnName(ContactsContract.CommonDataKinds.Relation.LABEL)
+    public abstract String label();
+
+    public static Relation create(Cursor cursor) {
+        return AutoValue_Relation.createFromCursor(cursor);
+    }
+
+
+    public enum Type {
+        CUSTOM(ContactsContract.CommonDataKinds.Relation.TYPE_CUSTOM),
+        ASSISTANT(ContactsContract.CommonDataKinds.Relation.TYPE_ASSISTANT),
+        BROTHER(ContactsContract.CommonDataKinds.Relation.TYPE_BROTHER),
+        CHILD(ContactsContract.CommonDataKinds.Relation.TYPE_CHILD),
+        DOMESTIC_PARTNER(ContactsContract.CommonDataKinds.Relation.TYPE_DOMESTIC_PARTNER),
+        FATHER(ContactsContract.CommonDataKinds.Relation.TYPE_FATHER),
+        FRIEND(ContactsContract.CommonDataKinds.Relation.TYPE_FRIEND),
+        MANAGER(ContactsContract.CommonDataKinds.Relation.TYPE_MANAGER),
+        MOTHER(ContactsContract.CommonDataKinds.Relation.TYPE_MOTHER),
+        PARENT(ContactsContract.CommonDataKinds.Relation.TYPE_PARENT),
+        PARTNER(ContactsContract.CommonDataKinds.Relation.TYPE_PARTNER),
+        REFERRED_BY(ContactsContract.CommonDataKinds.Relation.TYPE_REFERRED_BY),
+        RELATIVE(ContactsContract.CommonDataKinds.Relation.TYPE_RELATIVE),
+        SISTER(ContactsContract.CommonDataKinds.Relation.TYPE_SISTER),
+        SPOUSE(ContactsContract.CommonDataKinds.Relation.TYPE_SPOUSE);
+
+        int typeMap;
+
+        Type(int columnName) {
+            this.typeMap = columnName;
+        }
+
+        public int getTypeMap() {
+            return typeMap;
+        }
+    }
+
+
+}
--- kontact/src/main/kotlin/com/hackedcube/kontact/ContactUtils.kt
+++ kontact/src/main/kotlin/com/hackedcube/kontact/ContactUtils.kt
@@ -7,6 +7,7 @@ import android.database.Cursor
 import android.net.Uri
 import android.provider.ContactsContract
 
+
 fun Context.queryAllContacts(): List<Kontact> {
     contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null).use {
         return generateSequence { if (it.moveToNext()) it else null }
@@ -25,23 +26,41 @@ fun Context.getContactFromId(uri: Uri): Kontact? {
 private fun kontactFromCursor(context: Context, cursor: Cursor): Kontact {
     var kontact = Kontact.create(cursor)
 
+    // Fetch Phone Numbers
     if (kontact.hasPhoneNumber()) {
         context.contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", arrayOf(kontact.id()), null).use { phoneCursor ->
             val phoneNumbers = generateSequence { if (phoneCursor.moveToNext()) phoneCursor else null }
-                    .map { PhoneNumber.createfromCursor(it) }
+                    .map { PhoneNumber.create(it) }
                     .toList()
 
             kontact = kontact.withPhoneNumbers(phoneNumbers)
         }
     }
 
+    // Fetch Email addresses
     context.contentResolver.query(ContactsContract.CommonDataKinds.Email.CONTENT_URI, null, ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = ?", arrayOf(kontact.id()), null).use { emailCursor ->
         val emailAddresses = generateSequence { if (emailCursor.moveToNext()) emailCursor else null }
-                .map { EmailAddress.createfromCursor(it) }
+                .map { EmailAddress.create(it) }
                 .toList()
 
         kontact = kontact.withEmailAddresses(emailAddresses)
     }
+
+    // Fetch additional info
+    val where = String.format(
+            "%s = ? AND %s = ?",
+            ContactsContract.Data.MIMETYPE,
+            ContactsContract.CommonDataKinds.Relation.CONTACT_ID)
+
+    val whereParams = arrayOf(ContactsContract.CommonDataKinds.Relation.CONTENT_ITEM_TYPE, kontact.id()))
+
+    context.contentResolver.query(ContactsContract.Data.CONTENT_URI, null, where, whereParams, null).use { dataCursor ->
+        val relation = Relation.create(dataCursor)
+
+        kontact = kontact.withRelation(relation)
+    }
+
+
 
     return kontact
 }
